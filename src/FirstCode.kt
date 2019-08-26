b3e65f0caa8a3731ecae399247c5d4e326f0e348
Ganeshadsul <ganeshadsul76@gmail.com>
2019-08-26 21:01:49 +0530

    no message

---
+++ src/FirstCode.kt
@@ -0,0 +1,8 @@
+fun main()
+{
+    //print(10)     //To print numbers(Integers, Float, Double etc.)
+    print("Welcome to Kotlim")     //To print Strings
+    println("Ganesh")       //To print on next line
+
+
+}
