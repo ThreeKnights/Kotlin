1f2e449c48b7a2b5d5e36ef0862a633b97d279d2
Ganeshadsul <ganeshadsul76@gmail.com>
2019-12-13 14:42:53 +0530

    no message

--- src/Datatypes.kt
+++ src/Datatypes.kt
@@ -4,25 +4,23 @@ fun main()
     //It is not necessary to put datatype while declaring variable. Kotlin can automatically identify the datatype of the variable based on the data stored in variable.
     // 2 ways to define variables
 
-    /*
     //1. when you want to put data in variable in same line
     var str = "Welcome to Kotlin"
     println(str)
     println(str)
     println(str)
-    */
 
-    /*
+
     //2. When you want to put data in variable in next line or later
     var str1:String   // when you wan to put data in next line or in later you must define datatype of that variable
     str1 = "Abc Xyz"
     println(str1)
     println(str1)
     println(str1)
-    */
+
+
 
     //Simple Example using variables
-
     var fname = "Abc"
     var lname:String
     lname = "Xyz"
@@ -32,6 +30,5 @@ fun main()
     println("******User Info******")
     println("Name:"+fname+" "+lname)
     println("Age:"+age)
-    println("Result:$res") //you can also use variable inside double quote  by using $ sign in front of the variable
-
+    println("Result:$res") //you can also use variable inside double quote by using $ sign as a prefix to variable
 }
