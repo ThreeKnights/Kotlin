2ecc25b2a2edac2b1b201a482864145e26e4c621
Ganeshadsul <ganeshadsul76@gmail.com>
2020-01-07 03:37:06 +0530

    ArrayListDemo File Added.

---
+++ src/ArrayListDemo.kt
@@ -0,0 +1,40 @@
+fun main()
+{
+    //ArrayList is a dynamic array where you don't need to put size of array list
+    //If size of the ArrayList is 4 then for the 5th value in ArrayList it doubles the size of itself
+
+    //Declaring ArrayList
+    val pet:ArrayList<String> = ArrayList()
+    // Syntax-> val or var variable_name:ArrayList<Data_type_of_array> = ArrayList
+
+    //Accepting values in ArrayList
+   do {
+       print("Enter Pet Name or type quit to exit:")
+       val petName = readLine()!!.toString()
+       if(petName != "quit")
+           pet.add(petName)
+   }while (petName != "quit")
+
+    //to print that ArrayList using index
+    println("Print ArrayList using Index")
+    for (i in  0 until pet.size) //Also equal to 0..max-1
+    {
+        println("Pet Name: ${pet[i]}")
+    }
+
+    //to print that ArrayList using objects
+    println("Print ArrayList Using Objects")
+    for (i in  pet) //Also equal to 0..max-1
+    {
+        println("Pet Name: $i")
+    }
+
+    //search element in ArrayList
+    println("Searching Element in ArrayList")
+    print("Enter pet name to be searched:")
+    val petName = readLine()!!
+    if(pet.contains(petName))
+        println("You have $petName")
+    else
+        println("You don't have $petName")
+}
